e387d31ec967ff698488bec0cfd5773526ac4752
mr7-seven <94348725+mr7-seven@users.noreply.github.com>
2023-06-08 16:19:53 +0800

    Add files via upload

--- get_data.php
+++ get_data.php
@@ -1,37 +1,25 @@
 <?php
-header('Content-Type: application/json');
+// Konfigurasi database
+require_once('db_config.php');
 
-// baca data dari file dataku.json
-$data = json_decode(file_get_contents('dataku.json'), true);
+// Ambil data dari database
+$sql = "SELECT * FROM tb_data ORDER BY waktu DESC";
+$result = $conn->query($sql);
 
-// baca data dari request POST dalam format JSON
-$json = file_get_contents('php://input');
-$postData = json_decode($json, true);
+// Buat array untuk menampung data
+$data = array();
 
-// periksa apakah ada data POST yang diterima
-if (!empty($postData)) {
-  // ambil nilai suhu, kelembapan, dan lux dari data POST
-  $suhu = $postData['suhu'];
-  $kelembapan = $postData['kelembapan'];
-  $lux = $postData['lux'];
-
-  // update nilai suhu, kelembapan, dan lux di variabel $data
-  $data['suhu'] = $suhu;
-  $data['kelembapan'] = $kelembapan;
-  $data['lux'] = $lux;
-
-  // simpan kembali data ke dalam file dataku.json
-  $result = file_put_contents('dataku.json', json_encode($data, JSON_PRETTY_PRINT));
-
-  // cek apakah data berhasil diupdate
-  if ($result !== false) {
-    $response = array('status' => 'success', 'pesan' => 'Data berhasil diperbarui.');
-  } else {
-    $response = array('status' => 'error', 'pesan' => 'Terjadi kesalahan saat memperbarui data.');
-  }
-} else {
-  $response = array('status' => 'error', 'pesan' => 'Data tidak ditemukan atau permintaan tidak valid.');
+// Loop untuk menambahkan data ke dalam array
+if ($result->rowCount() > 0) {
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $data[] = $row;
+    }
 }
 
-// kirim response dalam format JSON
-echo json_encode($response);
+// Kembalikan data dalam bentuk JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+
+// Tutup koneksi
+$db = null;
+?>
