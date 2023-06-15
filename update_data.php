93ae1328bca7feb4f17dd5ebff03b5b700c36d70
mr7-seven <94348725+mr7-seven@users.noreply.github.com>
2023-06-15 18:22:00 +0800

    Create update_data.php
    update data saldo

---
+++ update_data.php
@@ -0,0 +1,28 @@
+<?php
+// Koneksi ke database
+include 'db_config.php';
+
+try {
+    // Ambil nilai input dari permintaan AJAX
+    $uid = $_POST['uid'];
+    $saldo = $_POST['saldo'];
+
+    // Lakukan query untuk update data
+    $query = "UPDATE tb_data SET saldo = :saldo WHERE uid = :uid";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':saldo', $saldo);
+    $stmt->bindParam(':uid', $uid);
+    // Ganti ':id' dengan nilai ID yang sesuai
+
+    // Eksekusi query
+    if ($stmt->execute()) {
+        $response = "Data berhasil diupdate";
+        echo $response;
+    } else {
+        $response = "Gagal mengupdate data";
+        echo $response;
+    }
+} catch (PDOException $e) {
+    $response = "Error: " . $e->getMessage();
+    echo $response;
+}
